f9c2958898ac7db308c5d811dad5e1bf0aad4d02
Prof-Laura <lala.sol2016@gmail.com>
2025-04-17 16:15:48 -0300

    Código de exemplo

--- POO/Aula 2 - Herança Simples e Múltipla/heranca_simples.py
+++ POO/Aula 2 - Herança Simples e Múltipla/heranca_simples.py
@@ -1,50 +1,55 @@
+### Herança Simples ###
 
-class veiculo():
-    def __init__(self, cor, placa, num_rodas):
+# Classe pai/base
+class Veiculo():
+    def __init__(self, placa, cor, num_rodas):
         self.cor = cor
         self.placa = placa
         self.num_rodas = num_rodas
 
+    # Métodos
     def ligar_motor(self):
-        text = "Ligando motor"
-        print(text)
+        print("Ligando o motor...")
 
     def status(self):
-        return f"Cor: {self.cor} - Placa: {self.placa} - Número de Rodas: {self.num_rodas}"
+        return f"Cor: {self.cor} - Placa: {self.placa} - Numero de Rodas: {self.num_rodas}"
     
-
-# herdo as características do pai sem sobescrever, ou seja, minha classe filha tem exatemente os mesmos atributos que a classe pai
-class moto(veiculo):
+# Começo a herança simples
+# Herdo as características do pai (Veiculo) sem sobrescrever, ou seja, minha classe filha (moto) tem exatamente os mesmos atributos que a classe pai
+class Moto(Veiculo):
     pass
 
-class carro(veiculo):
-    pass
-
-class caminhao(veiculo):
-    # método para sobescrever, pois quero característica do pai + algo novo/modificar algo
-    def __init__(self, carregado, cor, placa, num_rodas):
-            super().__init__(cor, placa, num_rodas) # chama o __init__ da classe pai 
-            self.carregado = carregado
+class Caminhao(Veiculo):
+    # Método para sobrescrever a classe pai (Veiculo), pois eu quero característica do pai + algo novo ou + modificar algo
+    def __init__(self, placa, cor, num_rodas, carregado):
+        super().__init__(placa, cor, num_rodas) # chamando o __init__ (construtor) da classe pai
+        self.carregado = carregado
 
     # Sem o super
-    """def __init__(self, carregado, cor, placa, num_rodas):
+    """def __init__(self, placa, cor, num_rodas, carregado):
         self.cor = cor
         self.placa = placa
-        self.num_rodas = num_rodas
-
-        self.carregado = carregado"""
-
+        self.num_rodas = num_rodas"""
+        
+    # Método da classe caminhão
     def esta_carregado(self):
         print(f"{"Sim" if self.carregado else "Não"} estou carregado")
-
-motocicleta = moto("preta", "abc", 2)
-
-carros = carro("vermelho", "ert", 4)
-
-caminhoes = caminhao(True, "branco", "qwe3", 8)
-caminhoes.ligar_motor()
-caminhoes.esta_carregado()
-
+        """if self.carregado == True:
+            print("Sim estou carregado")
+        else:
+            print("Não estou carregado")"""
+        
+## Instanciar novos objetos ##
+# Intanciando moto / classe sem sobrescrever
+print("-"*5 + "Usando a classe moto" + "-"*5)
+motocicleta = Moto("ABC-123", "Preta", 2)
 print(motocicleta.status())
-print(carros.status())
+motocicleta.ligar_motor()
+print()
+
+# Instanciando caminhao / classe sobrescrevendo
+print("-"*5 + "Usando a classe caminhao" + "-"*5)
+caminhoes = Caminhao("ABC-4321", "Branco", 8, True)
+caminhoes.ligar_motor()
 print(caminhoes.status())
+caminhoes.esta_carregado()
